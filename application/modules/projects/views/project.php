172608e4df26c4f2acaae5aa314c404f237bcfe3
Nick VanMeter <nickjvm@gmail.com>
2014-04-08 21:53:10 -0700

    Project detail corrections and analytics tracking

--- application/modules/projects/views/project.php
+++ application/modules/projects/views/project.php
@@ -3,7 +3,7 @@
 		<div class="span8 main-content">
 	<?php if (isset($project)) :?>
         <h2 class="title">
-            <?php e($project->title) ?>: <?php e($project->type);?>
+            <?php e($project->title) ?>
         </h2>
         <ul class="share-widget list-unstyled">
             <li>
@@ -30,9 +30,17 @@
             <?php endif; ?>
         </ul>
         <ul class="list-unstyled project-details">
-            <li>Organization: <?php print $this->user_model->find_meta_for($project->created_by)->organization;?></li>
-            <li>Estimated Hours: <?php print $project->hours;?></li>
-            <li>Materials Budget: <?php print $project->budget; ?></li>
+            <?php $author = $this->user_model->find_meta_for($project->created_by); ?>
+            <?php if(isset($author->website) && $author->website != false) { 
+               print "<li>Organization: ".anchor($author->website,$author->organization)."</li>";
+            } else {
+                print "<li>Organization: ".$author->organization."</li>";
+            }
+            ?>
+            <?php if($project->hours != "Help") {?>
+                <li>Estimated Hours: <?php print $project->hours;?></li>
+            <?php }?>
+            <li>Materials Budget: <?php print $project->budget == "other" ? $project->budget_specify : '$'.$project->budget; ?></li>
             <li>Audience: <?php print $project->audience;?></li>
             <li>Deadline: <?php print $project->deadlines;?></li>
             <li>Project Background:
@@ -41,7 +49,7 @@
                 </div>
             </li>
         </ul>
-        <?php if(! has_permission('Bonfire.ProjectBriefs.Apply')):
+        <?php if(has_permission('Bonfire.ProjectBriefs.Apply')):
                 if(!$current_user || ($current_user->meta->category == "creative" && $valid_applicant)):?>
         	       <a href="<?php print site_url('projects/project/'.$project->brief_id.'/apply');?>" class="<?php print $current_user ? 'magnific ' : '';?> btn btn-lg btn-primary">Volunteer for this project! <i class="fa fa-chevron-circle-right"></i></a>
                    <a class="btn btn-link" href="<?php print site_url('projects'); ?>">Find another project</a>
